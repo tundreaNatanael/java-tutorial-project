e5b1ee8408c37d7fa71914b85c974b21ba7bf92f
nathan <natanael.tundrea@clearviewsocial.com>
2026-09-19 13:23:36 +0300

    add: class TextFileReader

---
+++ src/TextFileReader.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+public class TextFileReader {
+
+    private final Path path;
+
+    public TextFileReader(String path) {
+        this.path = Path.of(path);
+    }
+
+    public String read() throws IOException {
+        return Files.readString(path);
+    }
+}
